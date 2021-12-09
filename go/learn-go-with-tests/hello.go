77c118aaafb996a1427cbae8e368253dc35e91ba
satoshi-nishinaka <satoshie.sp@gmail.com>
2021-12-09 11:58:53 +0900

    ステップ1 Hello world リファクタリングとテストが通るように修正

--- go/learn-go-with-tests/hello.go
+++ go/learn-go-with-tests/hello.go
@@ -4,13 +4,16 @@ import "fmt"
 
 const englishHellowPrefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return englishHellowPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
