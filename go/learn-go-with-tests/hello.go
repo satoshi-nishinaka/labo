c86807473f72a702e12261a7caba6c9d947f343c
satoshi-nishinaka <satoshie.sp@gmail.com>
2021-12-22 18:57:38 +0900

    フランス語を追加

--- go/learn-go-with-tests/hello.go
+++ go/learn-go-with-tests/hello.go
@@ -2,16 +2,26 @@ package main
 
 import "fmt"
 
-const englishHellowPrefix = "Hello, "
+const french = "French"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
-		return "Hola, " + name
+
+    prefix := englishHelloPrefix
+	switch language {
+      case french:
+        prefix = frenchHelloPrefix
+      case spanish:
+        prefix = spanishHelloPrefix
 	}
-	return englishHellowPrefix + name
+
+	return prefix + name
 }
 
 func main() {
