8e1b301e382a24d61cadee5a768ddd50c4b09166
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-01-12 19:39:48 +0900

    makeを使ったスライスの初期化

--- go/learn-go-with-tests/step4-array/sum_test.go
+++ go/learn-go-with-tests/step4-array/sum_test.go
@@ -1,6 +1,7 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
 
@@ -15,4 +16,14 @@ func TestSum(t *testing.T) {
         }
     })
 
+}
+
+func TestSumAll(t *testing.T) {
+
+    got := SumAll([]int{1, 2}, []int{0, 9})
+    want := []int{3, 9}
+
+    if !reflect.DeepEqual(got, want) {
+        t.Errorf("got %v want %v", got, want)
+    }
 }
