cb9bdd835d8819829b2f98ec247eb7effee4bafb
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-01-12 18:58:54 +0900

    スライスを使う

--- go/learn-go-with-tests/step4-array/sum.go
+++ go/learn-go-with-tests/step4-array/sum.go
@@ -1,9 +1,9 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
     sum := 0
     for _, number := range numbers {
-      sum += number
+        sum += number
     }
     return sum
 }
