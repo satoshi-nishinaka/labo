e97b488e102e143952090579320789b71d67cccd
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-01-19 09:58:42 +0900

    構造体を利用する

--- go/learn-go-with-tests/step5-structs-methods-interface/shapes.go
+++ go/learn-go-with-tests/step5-structs-methods-interface/shapes.go
@@ -1,9 +1,14 @@
 package main
 
-func Perimeter(width float64, height float64) float64 {
-    return (width + height) * 2
+type Rectangle struct {
+    Width float64
+    Height float64
 }
 
-func Area(width float64, height float64) float64 {
-  return width * height
+func Perimeter(rectangle Rectangle) float64 {
+    return (rectangle.Width + rectangle.Height) * 2
+}
+
+func Area(rectangle Rectangle) float64 {
+  return rectangle.Width * rectangle.Height
 }
