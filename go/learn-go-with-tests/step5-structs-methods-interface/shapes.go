6eb8dd3d149e94513f845c5a04c9693cddf5f32c
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-01-19 10:09:23 +0900

    メソッドを利用する

--- go/learn-go-with-tests/step5-structs-methods-interface/shapes.go
+++ go/learn-go-with-tests/step5-structs-methods-interface/shapes.go
@@ -1,14 +1,24 @@
 package main
 
-type Rectangle struct {
-    Width float64
-    Height float64
-}
+import "math"
 
 func Perimeter(rectangle Rectangle) float64 {
     return (rectangle.Width + rectangle.Height) * 2
 }
 
-func Area(rectangle Rectangle) float64 {
-  return rectangle.Width * rectangle.Height
+type Rectangle struct {
+    Width  float64
+    Height float64
+}
+
+func (r Rectangle) Area() float64  {
+    return r.Width * r.Height
+}
+
+type Circle struct {
+    Radius float64
+}
+
+func (c Circle) Area() float64  {
+    return c.Radius * c.Radius * math.Pi
 }
