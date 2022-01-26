516c032a23b16dfa8c6ed875e57d90602aefef2c
satoshi-nishinaka <satoshie.sp@gmail.com>
2022-01-26 18:27:52 +0900

    マイナスになる場合はnilを返す

--- go/learn-go-with-tests/step6-pointer-errors/wallet_test.go
+++ go/learn-go-with-tests/step6-pointer-errors/wallet_test.go
@@ -4,31 +4,36 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		got := wallet.Balance()
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		want := Bitcoin(10)
+		assertBalance(t, wallet, startingBalance)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
 		}
 	})
 }
